d073dd59bce76b5b8775d777d38f4f0dd85a6c19
TeaganSmith <teasmith@colostate.edu>
2024-10-05 21:29:30 -0500

    updated data

--- spatial.py
+++ spatial.py
@@ -3,17 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the monarch sightings dataset
-df = pd.read_csv('csv-files-data/2024/monarch_sightings_with_fips_cleaned.csv')
+df = pd.read_csv('csv-files-data/2018/monarch_sightings2018_with_fips.csv')
 
 # Ensure 'FIPS' is of type string
 df['FIPS'] = pd.to_numeric(df['FIPS'], errors='coerce').astype('Int64')  # Convert to integers
 df['FIPS'] = df['FIPS'].astype(str).str.zfill(5)  # Zero-pad FIPS codes
 
 # Keep the relevant columns
-df = df[['city', 'state', 'county', 'count', 'FIPS', 'cleaned_county']]
+df = df[['Town','State/Province','County','Count','Latitude','Longitude','FIPS']]
 
 # Aggregate the sightings count by FIPS
-df_county_sightings = df.groupby(['FIPS']).agg({'count': 'sum'}).reset_index()
+df_county_sightings = df.groupby(['FIPS']).agg({'Count': 'sum'}).reset_index()
 
 # Load the shapefile for counties
 counties = gpd.read_file('115795-V3/AgChange/AgChange/shapefiles/US_counties_2012_geoid.shp')
@@ -34,7 +34,7 @@ counties_sightings = counties_sightings[counties_sightings.is_valid]
 fig, ax = plt.subplots()
 
 # Plot the data and set the aspect ratio
-counties_sightings.plot(column='count', ax=ax, legend=True, linewidth=0.8, edgecolor='0.8', cmap='OrRd')
+counties_sightings.plot(column='Count', ax=ax, legend=True, linewidth=0.8, edgecolor='0.8', cmap='OrRd')
 
 # Set the aspect ratio to 'auto' to avoid aspect-related errors
 ax.set_aspect('auto')
@@ -42,7 +42,7 @@ ax.set_aspect('auto')
 # Add title
 ax.set_title('Monarch Sightings by County', fontsize=15)
 
-output_file = 'monarch_sightings_map_2024.png'  # Change the filename and extension as needed
+output_file = 'generated_maps/monarch_sightings_map_2018.png'  # Change the filename and extension as needed
 plt.savefig(output_file, dpi=300, bbox_inches='tight')
 
 
